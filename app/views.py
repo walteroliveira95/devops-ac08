2fc37eab5d76ede377a4f5c590f6dc15b9840214
Walter Augusto Feitosa de Oliveira <42360184+walteroliveira95@users.noreply.github.com>
2018-10-22 20:05:40 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from app.models import Curso
+from app.models import Curso, Vestibular
 from datetime import datetime
 
 def home(request):
@@ -63,3 +63,16 @@ def cadastro_cursos(request):
         })
     )
 
+def cadastro_vestibulares(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/cadastro_vestibulares.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Cadastro de vestibulares',
+            'vestibulares': Vestibular.objects.all(),
+            'year':datetime.now().year,
+        })
+
+    )
