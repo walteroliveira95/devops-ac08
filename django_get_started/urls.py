a99ca9f53567a3429fb6c2a8ce95adae5df0d6f6
Walter Augusto Feitosa de Oliveira <42360184+walteroliveira95@users.noreply.github.com>
2018-10-22 20:07:58 -0300

    Update urls.py
    inserir:
    url(r'^cadastro_vestibulares', 'app.views.cadastro_vestibulares', name='cadastro_vestibulares'),

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     url(r'^about', 'app.views.about', name='about'),
     url(r'^cadastro_cursos', 'app.views.cadastro_cursos', name='cadastro_cursos'),
     url(r'^login/$',
+    url(r'^cadastro_vestibulares', 'app.views.cadastro_vestibulares', name='cadastro_vestibulares'),    
         'django.contrib.auth.views.login',
         {
             'template_name': 'app/login.html',
